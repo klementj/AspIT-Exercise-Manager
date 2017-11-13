2298b96a94d7ff046d46b5edf792b550edd083d0
NoahBroJ <noahzarkbro@gmail.com>
2017-11-13 14:56:26 +0100

    Started working on LML editor

---
+++ frontend/LML-Editor.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Editor</title>
+    <link rel="stylesheet" href="">
+</head>
+<body>
+    <section class="wrapper">
+        <section class="editor-wrapper">
+            <div class="toolbar">
+                <section class="toolbar-section headers">
+                    <!--
+                    header1
+                    header2
+                    header3
+                    -->
+                </section>
+                <section class="toolbar-section simple-formatting">
+                    <!--
+                    cursive
+                    bold
+                    bold cursive
+                    -->
+                </section>
+                <section class="toolbar-section transforms">
+                    <!--
+                    list item
+                    codeblock
+                    footnote
+                    -->
+                </section>
+                <section class="toolbar-section embeds">
+                    <!--
+                    link
+                    image
+                    iframe
+                    -->
+                </section>
+                
+            </div>
+            
+            <textarea class="editor"></textarea>
+        </section>
+    </section>
+</body>
+</html>
