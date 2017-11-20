b21151f72b24af1c37e0c4e04b0b94b5c0d1e83a
NoahBroJ <noahzarkbro@gmail.com>
2017-11-20 14:48:18 +0100

    Almost finished LML translation functionality. Need to fix cursive and bold styling.

--- backend/js/nola.js
+++ backend/js/nola.js
@@ -1,6 +1,7 @@
 function LMLTranslate() {
     textarea = document.getElementById("LMLeditor");
     input = textarea.value;
+    output = "";
     inputLines = input.split(/\n/g);
     outputLines = Array(inputLines.length);
     
@@ -25,23 +26,33 @@ function LMLTranslate() {
                     }
                     else {
                         outputLines[i] = '<p class="codeblock">' + inputLines[i].slice(3);
+                        if (inputLines[i + 1].split(' ')[0] != ';;') {
+                            outputLines[i] += '</p>';
+                        }
                     }
                 }
                 else if (i == inputLines.length - 1) {
-                    outputLines[i] = inputLines[i].slice(3) + '</p>';
+                    if (inputLines[i - 1].split(' ')[0] != ';;') {
+                        outputLines[i] = '<p class="codeblock">' + inputLines[i].slice(3) + '</p>';
+                    }
+                    else {
+                        outputLines[i] = '<br>' + inputLines[i].slice(3) + '</p>';
+                    }
                 }
                 else if (i > 0 && i < inputLines.length - 1){
                     if (inputLines[i - 1].split(' ')[0] != ';;') {
                         if (inputLines[i + 1].split(' ')[0] != ';;'){
                             outputLines[i] = '<p class="codeblock">' + inputLines[i].slice(3) + '</p>'
                         }
-                        outputLines[i] = '<p class="codeblock">' + inputLines[i].slice(3);
+                        else {
+                            outputLines[i] = '<p class="codeblock">' + inputLines[i].slice(3);
+                        }
                     }
                     else if (inputLines[i + 1].split(' ')[0] != ';;') {
-                        outputLines[i] = inputLines[i].slice(3) + '</p>';
+                        outputLines[i] = '<br>' + inputLines[i].slice(3) + '</p>';
                     }
                     else {
-                        outputLines[i] = inputLines[i].slice(3);
+                        outputLines[i] = '<br>' + inputLines[i].slice(3);
                     }
                 }
                 break;
@@ -89,34 +100,71 @@ function LMLTranslate() {
             case '*':
                 if (i == 0){
                     if (i == inputLines.length - 1) {
-                        outputLines[i] = '<ul><li>' + inputLines[i].slice(3) + '</li></ul>';
+                        outputLines[i] = '<ul><li>' + inputLines[i].slice(2) + '</li></ul>';
                     }
                     else {
-                        outputLines[i] = '<p class="codeblock">' + inputLines[i].slice(3);
+                        outputLines[i] = '<ul><li>' + inputLines[i].slice(2) + '</li>';
+                        if (inputLines[i + 1].split(' ')[0] != '*') {
+                            outputLines[i] += '</ul>';
+                        }
                     }
                 }
                 else if (i == inputLines.length - 1) {
-                    outputLines[i] = inputLines[i].slice(3) + '</p>';
-                }
-                else if (i > 0 && i < inputLines.length - 1){
-                    if (inputLines[i - 1].split(' ')[0] != ';;') {
-                        if (inputLines[i + 1].split(' ')[0] != ';;'){
-                            outputLines[i] = '<p class="codeblock">' + inputLines[i].slice(3) + '</p>'
-                        }
-                        outputLines[i] = '<p class="codeblock">' + inputLines[i].slice(3);
-                    }
-                    else if (inputLines[i + 1].split(' ')[0] != ';;') {
-                        outputLines[i] = inputLines[i].slice(3) + '</p>';
+                    if (inputLines[i - 1].split(' ')[0] != '*') {
+                        outputLines[i] = '<ul><li>' + inputLines[i].slice(3) + '</li></ul>';
                     }
                     else {
-                        outputLines[i] = inputLines[i].slice(3);
+                        outputLines[i] = '<li>' + inputLines[i].slice(3) + '</li></ul>';
+                    }
+                }
+                else if (i > 0 && i < inputLines.length - 1){
+                    if (inputLines[i - 1].split(' ')[0] != '*') {
+                        if (inputLines[i + 1].split(' ')[0] != '*'){
+                            outputLines[i] = '<ul><li>' + inputLines[i].slice(2) + '</li></ul>'
+                        }
+                        else {
+                            outputLines[i] = '<ul><li>' + inputLines[i].slice(2) + '</li>';
+                        }
+                    }
+                    else if (inputLines[i + 1].split(' ')[0] != '*') {
+                        outputLines[i] = '<li>' + inputLines[i].slice(2) + '</li></ul>';
+                    }
+                    else {
+                        outputLines[i] = '<li>' + inputLines[i].slice(2) + '</li>';
                     }
                 }
                 break;
                 
             default:
+                outputLines[i] = '<p>' + inputLines[i] + '</p>';
                 break;
         }
-        console.log(outputLines[i]);
+        output += outputLines[i] + '\n';
     }
+    
+    output = output.replace(/\*\*(.*)\*\*/g, function(a, b) {
+        return '<strong>' + b + '</strong>';
+    });
+    
+    output = output.replace(/\*(.*)\*/g, function(a, b) {
+        return '<en>' + b + '</en>';
+    });
+    
+    output = output.replace(/\[\[(.*)\]\]/g, function(a, b) {
+        return '<code>' + b + '</code>';
+    });
+    
+    output = output.replace(/\{\{(.*)\}(.*)\}/g, function(a, b, c) {
+        return '<a href="' + c + '">' + b + '</a>';
+    });
+    
+    output = output.replace(/\$\$(.*)\$\$/g, function(a, b) {
+        return '<sup><span class="footnote">' + b + '</span></sup>';
+    });
+    
+    output = output.replace(/\#\{(.+)\{(.*)\}\}/g, function(a, b, c) {
+        return '<i style="color:' + b + '">' + c + '</i>';
+    });
+    
+    console.log(output);
 }
