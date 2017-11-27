60de84f6e88318d21da599a1c89e8b08f11e72a4
NoahBroJ <noahzarkbro@gmail.com>
2017-11-27 11:26:51 +0100

    JS functionality to pair footnotes and references

--- frontend/js/footnote.js
+++ frontend/js/footnote.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
-    var footnotes = $(".footnote");
+    var footnotes = $('.footnote');
+    var refs = $('.footref');
+    
     for (i = 0; i < footnotes.length; i++) {
-        
+        console.log(refs.length);
+        var id = footnotes[i].dataset.footnote;
+        var ref;
+        var self;
+        refs.each(function() {
+            self = $(this);
+            if (self.data("footref") == id) {
+                ref = self[0];
+                return false;
+            }
+        });
+        refs = refs.not(self);
+        console.log(footnotes[i].innerText + ' has the footnote reference number ' + ref.innerText);
     }
 });
