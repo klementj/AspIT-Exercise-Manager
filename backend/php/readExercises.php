c0f795c7d31a3a10c7d16aa425b9de3541cb0230
NoahBroJ <noahzarkbro@gmail.com>
2017-12-12 09:47:51 +0100

    Started exercise reading functionality

--- backend/php/readExercises.php
+++ backend/php/readExercises.php
@@ -1,3 +1,14 @@
 <?php
-
+/*Validate user logged in*/
+if (isset($_SESSION['userId'])) {
+    /*Get exercises from database*/
+    $sql = "SELECT * FROM exercises \n";
+    if ($_SESSION['accessLevel' < 2]) {
+        $sql += "WHERE AccessLevel = 1 "
+    }
+    $stmt = $dbh->prepare(
+        "SELECT * FROM exercises 
+        WHERE AccessLevel = 2"
+    );
+}
 ?>
