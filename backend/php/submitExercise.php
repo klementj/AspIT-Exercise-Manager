ed88dc485d7581d1ccbc5f8189661d5375343178
NoahBroJ <noahzarkbro@gmail.com>
2018-01-17 10:51:11 +0100

    Commenting

--- backend/php/submitExercise.php
+++ backend/php/submitExercise.php
@@ -6,11 +6,12 @@ session_start();
 
 /*Validate user accessLevel*/
 if ($_SESSION['accessLevel'] > 1) {
+    
     /*Close connection*/
     $dbh = null;
     
-    /*Redirect to other page with error*/
 } else {
+    
     /*Get form values*/
     $exerciseId = $_POST['exerciseId'];
     $subjectId = $_POST['subjectId'];
@@ -19,15 +20,19 @@ if ($_SESSION['accessLevel'] > 1) {
     
     try {
         if (empty($exerciseId)) {
+            /*If there is no exercise id, the exercise is completely new*/
             require "createExercise.php";
         }
         else {
             if (!IsAuthor($exerciseId, $_SESSION['userId'])) {
+                /*If current user is not on the author list, new exercise is added and user is added as latest author*/
                 require "transferExercise.php";
             } else {
                 if (GetLatestAuthorId($exerciseId) == $_SESSION['userId']) {
+                    /*If current user is latest author, simply update the exercise*/
                     require "updateThisExercise.php";
                 } else {
+                    /*If current user is not latest author but is on the author list, new exercise is added and user is added as latest author*/
                     require "transferExercise.php";
                 }
             }
