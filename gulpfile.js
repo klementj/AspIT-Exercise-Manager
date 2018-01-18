e0a81217f23f01c1c3b232e36e8a9406c57064bd
Klement Baastrup Johansen <kljo@campusvejle.dk>
2018-01-18 22:19:46 +0100

    Foundation for proper dev and deploy folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
+/*
+* --------------------- 
+* DEPLOY
+*
+* Upload only new files to FTP
+*
+* ---------------------
+*/
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var ftp = require('vinyl-ftp');
 
+// Create config.json with {"host":"HOSTNAME","user":"YOUR_USERNAME","password":"YOUR_PASSWORD"}
 var config = require('./config.json');
 
- 
 gulp.task('deploy', function () {
-
     var conn = ftp.create({
         host:       config.host,
         user:       config.user,
@@ -15,6 +22,7 @@ gulp.task('deploy', function () {
         log:        gutil.log
     });
 
+    // Define what to upload
     var globs = [
         'frontend/**',
         'backend/**',
@@ -24,7 +32,49 @@ gulp.task('deploy', function () {
     ];
 
     return gulp.src( globs, { base: '.', buffer: false})
+        // Name of folder to upload to
         .pipe( conn.newer('/KLJO/AspIT-Exercise-Manager'))
         .pipe( conn.dest('/KLJO/AspIT-Exercise-Manager'))
+});
 
+
+/*
+* --------------------- 
+* TRANSPILE
+*
+* Converts .scss to .css
+*
+* ---------------------
+*/
+var sass = require('gulp-sass');
+
+gulp.task('transpile', function () {
+    // Define source folder for .scss files
+    return gulp.src('frontend/scss/style.scss')
+      .pipe(sass())
+      
+      // Define destination folder for .css file
+      .pipe(gulp.dest('frontend/css'))
+});
+
+
+/*
+* --------------------- 
+* MINIFY
+*
+* Minifies .css and renames to .min.css
+*
+* ---------------------
+*/
+var minify = require('gulp-csso');
+var rename = require('gulp-rename');
+
+gulp.task('minify', function () {
+    // Define source folder for .css file
+    return gulp.src('frontend/css/*.css')
+      .pipe(minify())
+      .pipe(rename({suffix: '.min'}))
+
+      // Define destination folder for .min.css  
+      .pipe(gulp.dest('frontend/css'))
 });
